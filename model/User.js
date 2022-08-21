1446b34869c4bf2f600dbf65b9844a32ed65d654
VietHuy <hieppro138@gmail.com>
2022-08-21 15:42:08 +0700

    add update5

--- model/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PostSchema = mongoose.Schema({
-    account: String,
-    password: String,
-    username:String
-});
-
-module.exports = mongoose.model('User' , PostSchema);
